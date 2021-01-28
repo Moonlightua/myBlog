29fec86e88c8b1b5952dd9ccc3233ed7362da887
shY <kostyabohach@gmail.com>
2021-01-28 13:55:49 +0200

    Pagination

--- core/form/PaginationForm.php
+++ core/form/PaginationForm.php
@@ -11,24 +11,51 @@ class PaginationForm
     {
         echo '
             <div class="pagination">
-                <div>
-                   <a href="?page=1">First</a>
-                </div>
-                <div class="';
+            <div class="pag-items">
+                <div class="pag-item">
+                    <div class="';
+        echo ($page == 1) ? "pag-first disable" : "pag-first active";
 
-                echo ($page <= 1) ? "disable" : "active";
+
+        echo '">
+                   <a href="?page=1">First</a>
+                    </div>
+                </div>
+                
+                <div class="pag-item">
+                    <div class="';
+
+        echo ($page <= 1) ? "pag-prev disable" : "pag-prev active";
 
         echo'">
                   <a href="';
 
-                echo ($page <= 1) ? "#" : "?page=".($page - 1);
+        echo ($page <= 1) ? "#" : "?page=".($page - 1);
 
         echo'">Prev</a>
+                    </div>
                 </div>
                 
-                <div class="';
+                <div class="pag-item">
+                    <div class="pag-pages">
+                    ';
 
-        echo ($page >= $totalPages) ? "disable" : "active";
+        for ($i = 1; $i <= $totalPages; $i++){
+            if ($i == $page) {
+                echo "<div class='pag-pages active'><a href='?page=$i'>".$i."</a></div>";
+            } else {
+                echo "<div class='pag-pages off'><a href='?page=$i'>".$i."</a></div>";
+            }
+
+        }
+        echo'
+                    </div>
+                </div>
+                
+                <div class="pag-item">
+                    <div class="';
+
+        echo ($page >= $totalPages) ? "pag-next disable" : "pag-next active";
 
         echo'">
                   <a href="';
@@ -36,15 +63,23 @@ class PaginationForm
         echo ($page >= $totalPages) ? "#" : "?page=".($page + 1);
 
         echo'">Next</a>
+                    </div>
                 </div>
-                <div>
+                
+                <div class="pag-item">
+                <div class="';
+
+        echo ($page == $totalPages)? "pag-first disable" : "pag-first active";
+
+        echo '">
                    <a href="?page=';
 
         echo $totalPages;
 
         echo'">Last</a>
                 </div>
-                
+                </div>
+                </div>
             </div>';
 
     }
